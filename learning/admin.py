0fc4a841d07ce68bfb72797ebfd2506648facb3d
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-06-06 05:50:52 -0700

    register models

--- learning/admin.py
+++ learning/admin.py
@@ -4,4 +4,4 @@ from .models import Group, Message, Language
 # Register your models here.
 admin.site.register(Group)
 admin.site.register(Message)
-admin.site.register(Language)
+admin.site.register(Language) 
