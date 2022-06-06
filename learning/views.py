2d1c1402bb74d0bfbc2fd747d88d70c19166d9a6
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-06-06 04:05:20 -0700

    refactor templates

--- learning/views.py
+++ learning/views.py
@@ -2,8 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+groups = [
+    {'id':1, 'name':'Js'},
+    {'id':2, 'name':'Jsx'},
+    {'id':3, 'name':'Jsv'}
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'groups' : groups} #dictionary
+    return render(request, 'learning/home.html', context)
 
 def group(request):
-    return render(request, 'group.html')
+    return render(request, 'learning/group.html')
