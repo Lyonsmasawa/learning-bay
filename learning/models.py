44924a015798c7b8975779682931cd53ebd073f0
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-06-06 05:43:51 -0700

    group model

--- learning/models.py
+++ learning/models.py
@@ -1,5 +1,5 @@
-from turtle import update
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Group(models.Model):
@@ -11,3 +11,13 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name  #has to be a string if not convert str(value)
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True) #once
+    updated = models.DateTimeField(auto_now=True) #every time
+
+    def __str__(self):
+        return self.body[0:50]
--- learning/views.py
+++ learning/views.py
@@ -1,24 +1,17 @@
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Group
 
 # Create your views here.
-
-groups = [
-    {'id':1, 'name':'Js'},
-    {'id':2, 'name':'Jsx'},
-    {'id':3, 'name':'Jsv'}
-]
-
 def home(request):
+    groups = Group.objects.all()
+
     context = {'groups' : groups} #dictionary
     return render(request, 'learning/home.html', context)
 
 def group(request, pk):
-    group = None
-    for i in groups:
-        if i['id'] == int(pk):
-            group = i
+    group = Group.objects.get(id = pk)
 
     context = {'group' : group}
     return render(request, 'learning/group.html', context)
