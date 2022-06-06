25b28e1e6f9937349c29d5f01b17f5f114ed1c9c
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-06-05 23:27:14 -0700

    set up views

--- learning/views.py
+++ learning/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home page")
+    return render(request, 'home.html')
 
 def group(request):
-    return HttpResponse("groups")
+    return render(request, 'group.html')
