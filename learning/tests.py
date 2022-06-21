080189451c5dd3a2b0b0d12e3a94ca221aa02d4e
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-06-20 23:16:14 -0700

    test case

--- learning/tests.py
+++ learning/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Language, Group, Message
 
 # Create your tests here.
+class LanguageTestClass(TestCase):
+    def setUp(self):
+        self.new_language = Language(name="Lyons")
+        self.new_language.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_language, Language))
+
+class GroupTestCase(TestCase):
+    pass
